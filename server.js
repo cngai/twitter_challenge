dab81c77c728d0f632eab39cdcee29fff12ab634
Christopher Ngai <christopherngai@Christophers-MacBook-Pro-3.local>
2018-06-19 19:17:54 -0700

    added ejs file

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var path = require('path');
 
 var app = express();
 
 //set port number
 app.set('port', (process.env.PORT || 3000))
+
+//configure app
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 
 //middleware
 app.use(logger('dev'));
@@ -16,8 +23,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.get('/', function (req, res) {
-   res.send('Server running.');
-})
+   res.render('index');
+});
 
 app.listen(app.get('port'), function() {
 	console.log('Server started on port', app.get('port'))
